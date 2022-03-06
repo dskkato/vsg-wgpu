f262fb348456ecf7144aaafcf8694a3e82df8032
dskkato <kato.daisuke429@gmail.com>
2022-03-06 09:09:48 +0900

    update proto

--- vsg-messages/generate-java.ps1
+++ vsg-messages/generate-java.ps1
@@ -1,1 +1,2 @@
-protoc --java_out=vsg-messages-java/src/main/java -I=proto proto/message.proto
+rm -r -fo vsg-messages-java/src/main/java/*
+protoc --java_out=vsg-messages-java/src/main/java -Iproto proto/message.proto proto/shapes.proto
